d72b113d13df1bb16967bdf12dad0bf1f7afacc1
rostegg <rostik.nikolaenko@gmail.com>
2019-03-24 21:03:26 +0200

    Extended runners support

--- install.sh
+++ install.sh
@@ -4,6 +4,26 @@ clear
 echo -e "Copy \033[0;32micons8-docker.svg\033[0m to \033[0;32m/usr/share/icons/hicolor/scalable/apps\033[0m"
 cp icons8-docker.svg /usr/share/icons/hicolor/scalable/apps/
 
+function join_by { 
+    local IFS="$1"; shift; echo "$*"; 
+}
+
+function select_shortcut_name {
+    case $(basename $1) in
+     "run.sh")      
+          SHORTCUT_NAME=$(sed 's|'$INSTALL_SCRIPT_DIR'||g; s|/run.sh||g; s|/|-|g;' <<< $RUN_SCRIPT_PATH)
+          ;;
+     "kill.sh")      
+          SHORTCUT_NAME=$(sed 's|'$INSTALL_SCRIPT_DIR'||g; s|/kill.sh|-kill|g; s|/|-|g;' <<< $RUN_SCRIPT_PATH)
+          ;;
+    esac
+}
+
+declare -a RUNNERS_NAMES=("run.sh" "kill.sh")
+
+AVALIABLE_RUNNERS_REGEX=$(join_by \| "${RUNNERS_NAMES[@]}")
+
+
 SHORTCUT_TEMPLATE=$(cat <<-END
 [Desktop Entry]
 Version=1.0
@@ -20,13 +40,8 @@ INSTALL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && 
 
 while IFS=  read -r -d $'\0'; do
     RUN_SCRIPT_PATH=$(realpath $REPLY)
-    if [ $(basename ${RUN_SCRIPT_PATH}) == "run.sh" ] 
-    then
-        SHORTCUT_NAME=$(sed 's|'$INSTALL_SCRIPT_DIR'||g; s|/run.sh||g; s|/|-|g;' <<< $RUN_SCRIPT_PATH)
-    else
-        SHORTCUT_NAME=$(sed 's|'$INSTALL_SCRIPT_DIR'||g; s|/kill.sh|-kill|g; s|/|-|g;' <<< $RUN_SCRIPT_PATH)
-    fi
+    select_shortcut_name $RUN_SCRIPT_PATH
     echo -e "Created \033[0;32m${SHORTCUT_NAME}.desktop\033[0m shortcut in \033[0;32m${HOME}/Desktop\033[0m"
     echo "$(sed 's|@1|'$SHORTCUT_NAME'|g; s|@2|'$RUN_SCRIPT_PATH'|g' <<< $SHORTCUT_TEMPLATE)" > "${HOME}/Desktop/${SHORTCUT_NAME}.desktop"
     chmod +x ${HOME}/Desktop/${SHORTCUT_NAME}.desktop
-done < <(find . -regextype posix-egrep -regex ".*(run.sh|kill.sh)$" -print0)
+done < <(find . -regextype posix-egrep -regex ".*(${AVALIABLE_RUNNERS_REGEX})$" -print0)
