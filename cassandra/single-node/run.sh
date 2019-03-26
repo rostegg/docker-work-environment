b45f26ca17e0cbdbc6f2ff5401e1ccec59e98d62
rostegg <rostik.nikolaenko@gmail.com>
2019-03-26 19:42:36 +0200

    Cassandra support

--- cassandra/single-node/run.sh
+++ cassandra/single-node/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo docker run 
+clear
+
+read -e -p $'\e[32mRun as daemon? ("NO" by default):\e[0m ' DAEMON_MODE
+
+if [ -z "$DAEMON_MODE" ]
+then
+    docker-compose up 
+else
+    docker-compose up -d 
+fi
