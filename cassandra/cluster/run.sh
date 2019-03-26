cb2f43a2911bc7cdaa8affc84146fbcd7b6579cc
rostegg <rostik.nikolaenko@gmail.com>
2019-03-26 19:43:10 +0200

    Cassandra cluster support and CQLSH console

--- cassandra/cluster/run.sh
+++ cassandra/cluster/run.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# TODO improve network model
+
+clear
+
+read -e -p $'\e[32mRun as daemon? ("NO" by default):\e[0m ' DAEMON_MODE
+
+if [ -z "$DAEMON_MODE" ]
+then
+    docker-compose up 
+else
+    docker-compose up -d 
+fi
---
+++ cassandra/cqlsh/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm  cassandra cqlsh 192.168.0.101
