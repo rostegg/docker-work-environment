ba5f4c73c1eead02b5e347ad03f67cabf70b443f
rostegg <rostik.nikolaenko@gmail.com>
2019-03-27 21:56:10 +0200

    Fixed run script

--- elastic-search/cluster/run.sh
+++ elastic-search/cluster/run.sh
@@ -9,19 +9,11 @@ sudo sysctl -w vm.max_map_count=262144
 
 # gain permission for evere node data dir
 mkdir data
-chown 1000:1000 data
-chmod g+rwx data
-chgrp 1000 data
-
 mkdir data/lm_es_dev1_seed
-chown 1000:1000 data/lm_es_dev1_seed
-chmod g+rwx data/lm_es_dev1_seed
-chgrp 1000 data/lm_es_dev1_seed
-
 mkdir data/lm_es_dev2
-chown 1000:1000 data/lm_es_dev2
-chmod g+rwx data/lm_es_dev2
-chgrp 1000 data/lm_es_dev2
+chown 1000:1000 data*
+chmod g+rwx data*
+chgrp 1000 data*
 
 if [ -z "$DAEMON_MODE" ]
 then
