671ac5f9fd679763af7d2540b4bf5b6ed2a453d5
rostegg <rostik.nikolaenko@gmail.com>
2019-03-27 22:00:29 +0200

    Elasticsearch single node support

---
+++ elastic-search/single-node/kill.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker kill singlenode_lm_es_dev1_1
+
+read -e -p $'\e[31mRemove data folder? ("NO" by default):\e[0m ' REMOVE_DATA
+
+if [ ! -z "$REMOVE_DATA" ]
+then
+    DATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/data"
+    sudo rm -rf $DATA_DIR
+fi
---
+++ elastic-search/single-node/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+
+read -e -p $'\e[32mRun as daemon? ("NO" by default):\e[0m ' DAEMON_MODE
+
+# fix max virtual memory areas vm.max_map_count [65530] is too low
+sudo sysctl -w vm.max_map_count=262144
+
+# gain permission for evere node data dir
+mkdir data
+mkdir data/lm_es_dev1
+chown 1000:1000 data*
+chmod g+rwx data*
+chgrp 1000 data*
+
+if [ -z "$DAEMON_MODE" ]
+then
+    docker-compose up 
+else
+    docker-compose up -d 
+fi
