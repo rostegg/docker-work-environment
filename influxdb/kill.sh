da53530bbdb20d07efe992b93a302b9a9f43afde
rostegg <rostik.nikolaenko@gmail.com>
2019-03-28 13:52:42 +0200

    Influxdb support

---
+++ influxdb/kill.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker kill influxdb_chronograf_1
+docker kill influxdb_influxdb_1
+
+read -e -p $'\e[31mRemove data folder? ("NO" by default):\e[0m ' REMOVE_DATA
+
+if [ ! -z "$REMOVE_DATA" ]
+then
+    DATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/data"
+    sudo rm -rf $DATA_DIR
+fi
