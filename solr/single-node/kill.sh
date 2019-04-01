2675c9764d141bf0ca97db060be962c3e9b2bada
rostegg <rostik.nikolaenko@gmail.com>
2019-04-01 20:09:40 +0300

    Solr single node with default core

---
+++ solr/single-node/kill.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker kill cluster_lm_slr_dev1_1
+docker kill cluster_lm_slr_dev2_1
+docker kill cluster_lm_zoo_dev2_1
+docker kill cluster_lm_zoo_dev1_1
+
+read -e -p $'\e[31mRemove data folder? ("NO" by default):\e[0m ' REMOVE_DATA
+
+if [ ! -z "$REMOVE_DATA" ]
+then
+    DATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/data"
+    sudo rm -rf $DATA_DIR
+fi
